156affe223925c016900c5cff68b29b634b321d8
blackwelljerm <jermdsmith@yahoo.com>
2015-04-27 12:50:06 -0600

    Page 73

--- WingtipToysJerm/ProductList.aspx.cs
+++ WingtipToysJerm/ProductList.aspx.cs
@@ -4,14 +4,27 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using WingtipToysJerm.Models;
+using System.Web.ModelBinding;
 
 namespace WingtipToysJerm
 {
-    public partial class ProductList : System.Web.UI.Page
+    public partial class ProductList : Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        public IQueryable<Product> GetProducts([QueryString("id")] int? categoryID)
+        {
+            var db = new WingtipToysJerm.Models.ProductContext();
+            IQueryable<Product> query = db.Products;
+            if (categoryID.HasValue && categoryID > 0)
+	        {
+		    query = query.Where(p => p.CategoryID == categoryID);
+	        }
+            return query;
         }
     }
 }
